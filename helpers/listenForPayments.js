84ca2a76975df0ca23b96521d381958904a287b8
fomativeh <ufomaitive@gmail.com>
2024-05-24 21:07:25 +0100

    Added loop to payment listener

--- helpers/listenForPayments.js
+++ helpers/listenForPayments.js
@@ -22,88 +22,111 @@ const queue = new Queue({
 
 const listenForPayments = async () => {
   // Create a random signature so that the array of signatures will be present for looping
-  const randomSignatureCreated = await Signature.findOne({ signature: "randomSignature" });
+  const randomSignatureCreated = await Signature.findOne({
+    signature: "randomSignature",
+  });
   if (!randomSignatureCreated) {
-    const signatureForThisTransaction = new Signature({ signature: "randomSignature" });
+    const signatureForThisTransaction = new Signature({
+      signature: "randomSignature",
+    });
     await signatureForThisTransaction.save();
   }
 
-  // setInterval(async () => {
-    try {
-      // Get the confirmed signatures for the wallet address
-      const confirmedSignatures = await connection.getConfirmedSignaturesForAddress2(walletAddress, {
-        maxSupportedTransactionVersion: 0
+  try {
+    // Get the confirmed signatures for the wallet address
+    const confirmedSignatures =
+      await connection.getConfirmedSignaturesForAddress2(walletAddress, {
+        maxSupportedTransactionVersion: 0,
       });
-      const signatures = confirmedSignatures.map(({ signature }) => signature);
+    const signatures = confirmedSignatures.map(({ signature }) => signature);
 
-      // Add the signatures to the queue
-      signatures.forEach((signature) => {
-        queue.add(async () => {
-          try {
-            // Get the transaction for the signature
-            const transaction = await connection.getTransaction(signature, {
-              maxSupportedTransactionVersion: 0
-            });
+    // Add the signatures to the queue
+    signatures.forEach((signature) => {
+      queue.add(async () => {
+        try {
+          // Get the transaction for the signature
+          const transaction = await connection.getTransaction(signature, {
+            maxSupportedTransactionVersion: 0,
+          });
 
-            // Get the current SOL price in USD from Bitfinex
-            const res = await axios.get("https://api.bitfinex.com/v1/pubticker/solusd");
-            const currentSolPriceInUsd = res.data.last_price;
+          // Get the current SOL price in USD from Bitfinex
+          const res = await axios.get(
+            "https://api.bitfinex.com/v1/pubticker/solusd"
+          );
+          const currentSolPriceInUsd = res.data.last_price;
 
-            // Access each transaction
-            if (transaction !== undefined) {
-              const amount = (transaction.meta.postBalances[1] - transaction.meta.preBalances[1]) / LAMPORTS_PER_SOL; // Convert to SOL units
-              // const senderAddress = transaction.transaction.message.accountKeys[0].toString();
-              if(transaction.transaction.message.accountKeys){
-              const senderAddress = transaction.transaction.message.accountKeys[0].toString();
+          // Access each transaction
+          if (transaction !== undefined) {
+            const amount =
+              (transaction.meta.postBalances[1] -
+                transaction.meta.preBalances[1]) /
+              LAMPORTS_PER_SOL; // Convert to SOL units
+            // const senderAddress = transaction.transaction.message.accountKeys[0].toString();
+            if (transaction.transaction.message.accountKeys) {
+              const senderAddress =
+                transaction.transaction.message.accountKeys[0].toString();
               // console.log(amount, "sent by", senderAddress)
-                
+
               // Check if transaction has been saved
               const allSignatures = await Signature.find();
               if (allSignatures.length > 0) {
-                const signatureExists = allSignatures.find((eachSignature) => eachSignature.signature == signature);
+                const signatureExists = allSignatures.find(
+                  (eachSignature) => eachSignature.signature == signature
+                );
 
                 // Store transaction and credit user if it hasn't been saved in db
                 if (!signatureExists) {
-                  const signatureForThisTransaction = new Signature({ signature });
+                  const signatureForThisTransaction = new Signature({
+                    signature,
+                  });
                   await signatureForThisTransaction.save();
 
-                  const sender = await User.findOne({ walletAddress: senderAddress });
+                  const sender = await User.findOne({
+                    walletAddress: senderAddress,
+                  });
                   if (sender) {
                     // console.log(sender, "credited", amount)
                     // console.log(sender, "sent", amount)
                     const creditAmount = currentSolPriceInUsd * amount;
-                  // console.log(`${sender}, ${amount} sol, ${creditAmount} usd`);
+                    // console.log(`${sender}, ${amount} sol, ${creditAmount} usd`);
                     const previousBalance = sender.balance;
-                    const newBalance = parseFloat((previousBalance + creditAmount).toFixed(2));
-                    await User.findOneAndUpdate({ walletAddress: senderAddress }, { balance: newBalance });
+                    const newBalance = parseFloat(
+                      (previousBalance + creditAmount).toFixed(2)
+                    );
+                    await User.findOneAndUpdate(
+                      { walletAddress: senderAddress },
+                      { balance: newBalance }
+                    );
 
                     // Create new credit notification
                     const creditNotification = new Notification({
                       message: `Deposit confirmed ✅ 
 Amount: *${amount} sol*
-*$${parseFloat(creditAmount.toFixed(2))}* ([Using bitfinex rate](https://trading.bitfinex.com/t/SOL:USD?type=exchange))
+*$${parseFloat(
+                        creditAmount.toFixed(2)
+                      )}* ([Using bitfinex rate](https://trading.bitfinex.com/t/SOL:USD?type=exchange))
 New Balance: *$${newBalance}*`,
                       chatId: sender.chatId,
                     });
                     await creditNotification.save();
 
                     //Delete the signature after sending the alert
-                    await Signature.deleteOne(signatureForThisTransaction)
+                    await Signature.deleteOne(signatureForThisTransaction);
                   }
                 }
               }
-              }
-
             }
-          } catch (error) {
-            handleError(null, error);
           }
-        });
+        } catch (error) {
+          handleError(null, error);
+        }
       });
-    } catch (error) {
-      handleError(null, error);
-    }
-  // }, 5000); // 5 second interval
+    });
+  } catch (error) {
+    handleError(null, error);
+  }
+
+  setTimeout(listenForPayments, 5000);
 };
 
 module.exports = listenForPayments;
